07843efa1ac61fed9536b50e3d253289c60ff907
jsolomko <jsolomko@bk.ru>
2021-12-22 12:32:46 +0300

    ц

--- app/src/main/java/com/example/oneviewroomapp/MainActivity.java
+++ app/src/main/java/com/example/oneviewroomapp/MainActivity.java
@@ -1,5 +1,6 @@
-package com.example.oneviewroomapp.ui;
+package com.example.oneviewroomapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -7,13 +8,18 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import com.example.oneviewroomapp.R;
 import com.example.oneviewroomapp.entities.Word;
 import com.example.oneviewroomapp.db.WordListAdapter;
 import com.example.oneviewroomapp.db.WordViewModel;
+import com.example.oneviewroomapp.ui.PushActivity;
+import com.example.oneviewroomapp.ui.SettingActivity;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -42,6 +48,33 @@ public class MainActivity extends AppCompatActivity {
             //
             adapter.submitList(words);
         });
+    }
+
+
+    //МЕНЮ
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu, menu);
+        return true;
+    }
+    //Листнер нажатий в меню
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_push:
+                Intent intent = new Intent(MainActivity.this, PushActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.item2:
+                Intent i = new Intent(MainActivity.this, SettingActivity.class);
+                startActivity(i);
+                return true;
+            case R.id.item3:
+                Toast.makeText(this, "PIP", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void load(View view) {
@@ -75,8 +108,7 @@ public class MainActivity extends AppCompatActivity {
         mWordViewModel.delete();
     }
 
-    public void toPush(View view) {
-        Intent i = new Intent(MainActivity.this, PushActivity.class);
-        startActivity(i);
-    }
+
+
+
 }
---
+++ app/src/main/java/com/example/oneviewroomapp/ui/SettingActivity.java
@@ -0,0 +1,34 @@
+package com.example.oneviewroomapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.TableLayout;
+
+import com.example.oneviewroomapp.R;
+import com.example.oneviewroomapp.db.WordListAdapter;
+import com.example.oneviewroomapp.db.WordViewModel;
+
+public class SettingActivity extends AppCompatActivity {
+    private WordViewModel mWordViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_setting);
+        final WordListAdapter adapter = new WordListAdapter(new WordListAdapter.WordDiff());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mWordViewModel = new ViewModelProvider(this).get(WordViewModel.class);
+        mWordViewModel.getAllWords().observe(this, words -> {
+            // Update the cached copy of the words in the adapter.
+            //
+            adapter.submitList(words);
+        });
+    }
+}
